0517124be8b308f01a524d92b9a095f0d65078b3
iMac-14 <imac-14@iMac-14deiMac.local>
2025-06-10 16:16:48 +0800

    add func func GetUid(ctx context.Context) (int, error)

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# jwt
+# jwt4
+
+```bash
+go get github.com/scott-x/jwt/v4@4.0.2
+```
+
+### api
+
+- `func GenerateJWTToken(secret string, expires time.Duration, userID int64) (string, error)`: Generate JWT token if login successfuly
+- `func Auth(jwtSecret string, whiteList []string) middleware.Middleware`: jwt middleware
+- `func GetUid(ctx context.Context) (int, error)`: get user id
--- middleware_auth.go
+++ middleware_auth.go
@@ -2,6 +2,7 @@ package jwt4
 
 import (
 	"context"
+	"strconv"
 	"strings"
 
 	"github.com/go-kratos/kratos/v2/errors"
@@ -10,9 +11,14 @@ import (
 	"github.com/go-kratos/kratos/v2/transport/http"
 )
 
+// Define a private type to use as a context key to avoid conflicts.
+type contextKey string
+
 const (
 	authorizationKey = "Authorization"
 	bearerWord       = "Bearer"
+	// Change uid to private context key
+	contextUserIDKey contextKey = "user_id"
 )
 
 func Auth(jwtSecret string, whiteList []string) middleware.Middleware {
@@ -38,10 +44,40 @@ func Auth(jwtSecret string, whiteList []string) middleware.Middleware {
 					}
 
 					// save user_id to context
-					ctx = context.WithValue(ctx, "user_id", claims.UserID)
+					ctx = context.WithValue(ctx, contextUserIDKey, claims.UserID)
 				}
 			}
 			return handler(ctx, req)
 		}
 	}
+}
+
+
+// GetUid:  get user_id from context
+// Return an error if the user_id does not exist or its type is mismatched
+func GetUid(ctx context.Context) (int, error) {
+	val := ctx.Value(contextUserIDKey) // Access the value via the private userContextKey
+
+	if val == nil {
+		// user_id is absent from the context. This may be due to unexecuted middleware or a failed authentication.
+		return 0, errors.Unauthorized("UNAUTHORIZED", "user_id not found in context")
+	}
+
+	// Try to assert the value as an int type
+	// If claims.UserID is a string type, you'll need to convert int here
+	if uid, ok := val.(int); ok {
+		return uid, nil
+	}
+
+	// If the user_id is a string type, convert it
+	if uidStr, ok := val.(string); ok {
+		uid, err := strconv.Atoi(uidStr)
+		if err != nil {
+			return 0, errors.InternalServer("INTERNAL_ERROR", "invalid user_id format in context")
+		}
+		return uid, nil
+	}
+
+	// If the type doesn't match int or string
+	return 0, errors.InternalServer("INTERNAL_ERROR", "user_id in context is not of expected type")
 }
